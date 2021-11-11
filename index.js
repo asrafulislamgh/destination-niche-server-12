b0a875feb8449531a383255a902e99681bf4ef39
asrafulislamgh <gdmamun@yahoo.com>
2021-11-12 01:17:59 +0600

    admin panel is updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const { MongoClient } = require("mongodb");
 const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -25,6 +26,7 @@ async function run() {
     const database = client.db("destination_db");
     const serviceCollection = database.collection("services");
     const orderCollection = database.collection("orders");
+    const userCollection = database.collection("users");
 
     // GET API
     app.get("/properties", async (req, res) => {
@@ -32,13 +34,26 @@ async function run() {
       const result = await cursor.toArray();
       res.json(result);
     });
+    app.get("/users", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.json(result);
+      console.log(result);
+    });
 
     // My order getting
     app.get("/orders", async (req, res) => {
       const email = req.query.email;
-      const query = { email: email };
-      const cursor = orderCollection.find(query);
-      const result = await cursor.toArray();
+      let query, cursor, result;
+      if (email) {
+        query = { email: email };
+        cursor = orderCollection.find(query);
+        result = await cursor.toArray();
+      } else {
+        cursor = orderCollection.find({});
+        result = await cursor.toArray();
+      }
       console.log(cursor);
       // console.log(result);
       res.json(result);
@@ -49,6 +64,38 @@ async function run() {
       const user = req.body;
       const result = await orderCollection.insertOne(user);
       console.log(result);
+      res.json(result);
+    });
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.json(result);
+    });
+
+    // Delete API
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Update pending api
+    app.put("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await orderCollection.updateOne(filter, {
+        $set: { status: 1 },
+      });
+      res.json(result);
+    });
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const filter = { email: email };
+      const result = await userCollection.updateOne(filter, {
+        $set: { role: "admin" },
+      });
       res.json(result);
     });
   } finally {
